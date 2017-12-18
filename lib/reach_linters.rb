43f7f871ca1d460107e6f3631681ab8e44c84a57
Tyler Crocker <crocker.tylerj@gmail.com>
2017-12-18 11:16:39 -0800

    moving some requires to hopefully fix project loading

--- lib/reach_linters.rb
+++ lib/reach_linters.rb
@@ -1,9 +1,9 @@
-module ReachLinters
-  # Your code goes here...
-end
-
 require 'guard-brakeman'
 require 'guard-bundler-audit'
 require 'rubocop'
+
+module ReachLinters
+  # Your code goes here...
+end
 
 require "reach_linters/railtie" if defined?(Rails)
