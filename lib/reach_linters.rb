51982b6b102a517b03ee07b4940495b1443ab00c
Tyler Crocker <crocker.tylerj@gmail.com>
2017-12-18 12:02:06 -0800

    removing the two guard gem reqs

--- lib/reach_linters.rb
+++ lib/reach_linters.rb
@@ -1,9 +1,7 @@
-require 'guard-brakeman'
-require 'guard-bundler-audit'
-require 'rubocop'
-
 module ReachLinters
   # Your code goes here...
 end
+
+require 'rubocop'
 
 require "reach_linters/railtie" if defined?(Rails)
